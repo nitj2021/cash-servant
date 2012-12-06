101a528c6f1540672627e1d416931002087bb50b
hp <hp@ubuntu.ubuntu-domain>
2012-12-06 07:06:33 +0530

    production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Cash::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
+config.active_support.deprecation = :silence
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
